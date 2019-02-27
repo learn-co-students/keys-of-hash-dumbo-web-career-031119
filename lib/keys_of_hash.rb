8466d99b4614c5c82e2290dd0b2fd8a008c6103a
siberry <siberry226@gmail.com>
2019-02-27 10:30:47 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,19 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    ans = []
+    arguments.collect do |x|
+      self.collect do |y,z|
+        if arguments.include?(z)
+          ans << y
+        end
+      end
+    end
+    ans.uniq
   end
 end
+
+# x is argument(s)which are values, givens
+#y is key, z is value of y (e.g. y => z)
+# self.collect goes through each entry of hash
