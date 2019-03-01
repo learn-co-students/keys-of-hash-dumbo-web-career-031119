cd1773f32db256587e5aeb21dd687325de9fa92d
DanFCo <danfcorreia@gmail.com>
2019-03-01 13:56:54 -0500

    keys of hash

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,58 @@
+
+require"pry"
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+  returned_array = []
+    arguments.each do |place|
+      self.each do |k,v|
+
+        if place == v
+          returned_array << k
+
+        end
+      end
+    end
+    returned_array
   end
 end
+
+
+
+
+
+
+
+
+
+
+# require"pry"
+# class Hash
+#   def keys_of(*arguments)
+#     (the operating instance) self / {a: "1", b: "2"}.
+#
+#
+#
+#   end
+# end
+#
+# {a: "1", b: "3"}.keys_of
+
+
+# class Person
+#   def name
+#     @name
+#   end
+#   def initialize(name)
+#     @name = name
+#   end
+#   def sayHello
+#     puts "Hey my name is #{b.name}"
+#   end
+# end
+#
+# a = Person.new("jake")
+# b = Person.new("john")
+# a.sayHello
+# b.sayHello
+#
+# a.sayHello
