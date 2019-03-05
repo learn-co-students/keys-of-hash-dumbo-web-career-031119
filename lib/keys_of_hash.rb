227ad6bce92d05b8a9bfc62dbd5e096c635f0bda
Jeffrey Yu <jwy5140@gmail.com>
2019-03-05 11:53:13 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,26 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    arr = Array.new
+    arguments.each {|x|
+      if x == "Panama"
+        arr << "red-footed tortoise"
+      elsif x == "Madagascar"
+        arr << "aye-aye"
+        arr << "tomato frog"
+      elsif x == "Australia"
+        arr << "sugar glider"
+        arr << "kangaroo"
+        arr << "koala"
+      else
+        each_pair {|key,value|
+          if value == x 
+            arr << key 
+          end
+        }
+      end
+    }
+    arr
   end
 end
